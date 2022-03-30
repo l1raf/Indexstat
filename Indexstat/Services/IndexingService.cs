67a3d144b547b0faba7c9d6e8bfa18218696c227
Sabiyat <kssabiyat@gmail.com>
2022-03-30 22:51:05 +0300

    Deal with chunked response

--- Indexstat/Services/IndexingService.cs
+++ Indexstat/Services/IndexingService.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Indexstat.DTOs;
 using Indexstat.SerpApi;
 
@@ -5,8 +6,8 @@ namespace Indexstat.Services;
 
 public class IndexingService : IIndexingService
 {
-    private readonly ISerpApiSearch _search;
     private readonly HttpClient _httpClient;
+    private readonly ISerpApiSearch _search;
 
     public IndexingService(ISerpApiSearch search, HttpClient httpClient)
     {
@@ -47,27 +48,36 @@ public class IndexingService : IIndexingService
             return (ex.Message, null);
         }
     }
-    
+
     public async Task<(string? error, string? source)> GetPageSource(Uri uri, Uri cssFileAddress)
     {
         try
         {
-            _httpClient.DefaultRequestHeaders.Add("User-Agent", 
+            _httpClient.DefaultRequestHeaders.Add("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36");
             // _httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
 
             var response = await _httpClient.GetAsync(uri);
 
             response.EnsureSuccessStatusCode();
-            
-            var data = await response.Content.ReadAsStringAsync();
 
-            if (data.IndexOf("<base href", StringComparison.Ordinal) < 0)
+            string data;
+
+            if (response.Headers.TryGetValues("Transfer-Encoding", out IEnumerable<string>? headerValues)
+                && headerValues.Any(x => x.Equals("chunked", StringComparison.InvariantCultureIgnoreCase)))
             {
-                data = data.IndexOf("</title>", StringComparison.Ordinal) < 0 
-                    ? data.Replace("<head>",$"<head><base href=\"{uri}\"/>") 
-                    : data.Replace("</title>",$"</title><base href=\"{uri}\"/>");   
+                data = ReadChunkedResponse(await response.Content.ReadAsStreamAsync());
             }
+            else
+            {
+                data = await response.Content.ReadAsStringAsync();
+            }
+            
+            
+            if (data.IndexOf("<base href", StringComparison.Ordinal) < 0)
+                data = data.IndexOf("</title>", StringComparison.Ordinal) < 0
+                    ? data.Replace("<head>", $"<head><base href=\"{uri}\"/>")
+                    : data.Replace("</title>", $"</title><base href=\"{uri}\"/>");
 
             data = data.Replace("</head>",
                 $"<link rel=\"stylesheet\" href=\"{cssFileAddress}\"/></head>");
@@ -81,5 +91,12 @@ public class IndexingService : IIndexingService
         {
             return ("Error loading page", null);
         }
+    }
+
+    private string ReadChunkedResponse(Stream responseStream)
+    {
+        var reader = new StreamReader(responseStream, Encoding.UTF8);
+        var data = Encoding.UTF8.GetBytes(reader.ReadToEnd());
+        return Encoding.Default.GetString(data.ToArray());
     }
 }
